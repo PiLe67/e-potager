5ecc3cc8ba0241bff23bfd36b5d557ebe2bf3364
keiz44l <keiz44l@protonmail.com>
2024-11-20 10:37:47 +0100

    add firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getDatabase, provideDatabase } from '@angular/fire/database';
 
 @NgModule({
   declarations: [
@@ -12,7 +15,11 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    provideFirebaseApp(() => initializeApp({"projectId":"e-potager","appId":"1:655590651191:web:c1e918647ee650a08f96b2","databaseURL":"https://e-potager-default-rtdb.europe-west1.firebasedatabase.app","storageBucket":"e-potager.firebasestorage.app","apiKey":"AIzaSyAq8noe3P5wu6BMWRnQG3yA4Wf2RLM_Ebk","authDomain":"e-potager.firebaseapp.com","messagingSenderId":"655590651191"})),
+    provideAuth(() => getAuth()),
+    provideDatabase(() => getDatabase())
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
